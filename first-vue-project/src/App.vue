022132bbffe6aa263e7b357d86e9add902bd786a
Muhammad Arsy Al-Fahd <muhammadarsyalfahd@gmail.com>
2026-02-18 08:46:23 +0800

    initialize repo

---
+++ first-vue-project/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>Belajar Vue JS</h1>
+
+    <!-- implementasi interpolation -->
+    <h2>{{ nama }}</h2>
+    <!-- contoh attribute binding -->
+    <img
+      :src="gambar"
+      :alt="`Foto profil ${nama}`"
+      title="Foto profil"
+      width="250px"
+      height="250px"
+    />
+    <p>Umur: {{ umur }}</p>
+
+    <br /><br />
+
+    <!-- contoh event binding -->
+    <button @click="tambahUmur">Tambah Umur</button>
+
+    <br /><br />
+
+    <!-- contoh conditional rendering -->
+    <p v-if="umur >= 18">Status: Dewasa</p>
+    <p v-else>Status: Anak-anak</p>
+
+    <br />
+
+    <!-- implementasi two-way binding menggunakan v-model -->
+    <label>Masukkan pesan:</label>
+    <input type="text" v-model="pesan" />
+    <!-- implementasi interpolation -->
+    <p>Pesan kamu: {{ pesan }}</p>
+
+    <br />
+
+    <h3>Daftar Hobi:</h3>
+    <!-- contoh implementasi v-for -->
+    <ul>
+      <li v-for="(hobi, index) in daftarHobi" :key="index">
+        {{ hobi }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      nama: "Muhammad Arsy Al-Fahd",
+      umur: 20,
+      gambar: "../assets/pfp.jpg",
+      pesan: "",
+      daftarHobi: ["Ngoding", "Gaming"],
+    };
+  },
+  methods: {
+    tambahUmur() {
+      this.umur++;
+    },
+  },
+};
+</script>
